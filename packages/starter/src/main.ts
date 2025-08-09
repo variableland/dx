19570af3b8500314920b7a31e54747b0044352f9
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2025-08-08 22:14:59 -0500

    chore(deps): move to biome v2

--- packages/starter/src/main.ts
+++ packages/starter/src/main.ts
@@ -1,4 +1,4 @@
-import { type Options, createProgram } from "./program";
+import { createProgram, type Options } from "./program";
 import { logger } from "./services/logger";
 
 export async function main(options: Options) {
