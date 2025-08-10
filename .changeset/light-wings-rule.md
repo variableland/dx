93a863f41e6b19789a4aea8c45de7fde3a75774a
rcrd <raquirozbazan@gmail.com>
2025-08-09 19:02:05 -0500

    feat: expose biome bin as command (#99)

---
+++ .changeset/light-wings-rule.md
@@ -0,0 +1,5 @@
+---
+"@vlandoss/run-run": patch
+---
+
+Expose internal tools as the following command: `rr tools biome`
---
+++ packages/run-run/src/program/commands/tools.ts
@@ -0,0 +1,25 @@
+import { createCommand } from "commander";
+import type { Context } from "~/services/ctx";
+
+export function createToolsCommand(ctx: Context) {
+  function createToolCommandAction(toolBin: string) {
+    return async function toolAction(_: unknown, { args }: { args: string[] }) {
+      await ctx.shell.$`${toolBin} ${args.join(" ")}`;
+    };
+  }
+
+  function createToolCommand(toolBin: string) {
+    return createCommand(toolBin)
+      .helpCommand(false)
+      .helpOption(false)
+      .allowExcessArguments(true)
+      .allowUnknownOption(true)
+      .action(createToolCommandAction(toolBin));
+  }
+
+  return createCommand("tools")
+    .description("expose the internal tools 🛠️")
+    .addCommand(createToolCommand("biome"))
+    .addCommand(createToolCommand("tsc"))
+    .addCommand(createToolCommand("rimraf"));
+}
