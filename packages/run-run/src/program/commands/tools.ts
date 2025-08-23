8b77fc6acba03c596c1a3bda7c184f5086a6a828
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2025-08-23 15:46:44 -0500

    wip

--- packages/run-run/src/program/commands/tools.ts
+++ packages/run-run/src/program/commands/tools.ts
@@ -3,7 +3,7 @@ import { BiomeService } from "~/services/biome";
 import type { Context } from "~/services/ctx";
 
 function createToolCommand(toolBin: string) {
-  // biome-ignore format: I prefer multiline here
+  // biome-ignore format: I prefer multi-line here
   return createCommand(toolBin)
     .helpCommand(false)
     .helpOption(false)
